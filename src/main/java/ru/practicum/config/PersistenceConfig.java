c5585b894137809ecfb0b78104466cd535efa0b8
SergeyPrisyazhnyuk <130796016+SergeyPrisyazhnyuk@users.noreply.github.com>
2023-11-13 22:10:43 +0300

    feat : repo

--- src/main/java/ru/practicum/config/PersistenceConfig.java
+++ src/main/java/ru/practicum/config/PersistenceConfig.java
@@ -19,8 +19,6 @@ import java.util.Properties;
 @EnableTransactionManagement
 @EnableJpaRepositories(basePackages = "ru.practicum")
 @PropertySource(value = "classpath:application.properties")
-@EnableTransactionManagement // включает управление транзакциями и обработку соответствующих аннотаций
-@EnableJpaRepositories(basePackages = "ru.practicum")
 public class PersistenceConfig {
     private final Environment environment;
 
